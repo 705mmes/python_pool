70411d2183a350e9a4dc94971753ca797fc6c303
Samuel Munio <smunio@k2r3p7.42mulhouse.fr>
2024-10-30 13:07:03 +0100

    finished

--- python_pool_00/ex08/Loading.py
+++ python_pool_00/ex08/Loading.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-?
 
-import sys
 
-
-def main():
-
-
-if __name__ == "__main__":
-    main()
+def ft_tqdm(lst: range) -> None:
+    total = len(lst)
+    for i in lst:
+        percent = (i + 1) / total
+        bar_length = 75
+        filled_length = int(bar_length * percent)
+        bar = "=" * filled_length + "-" * (bar_length - filled_length)
+        print(f"\r{int(percent * 100)}%|{bar}| {i + 1}/{total}", end="")
+        yield i
